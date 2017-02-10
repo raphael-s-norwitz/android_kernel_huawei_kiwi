077db7b5793b20228e32fa4ff1acc5ac8614c25f
raphael-s-norwitz <raphael.s.norwitz@gmail.com>
2017-02-10 18:38:46 -0500

    updated build_kernel with apropriate configs so it compiles. Hopefully it'll flashgit add build_kernel.sh git add build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,6 +3,6 @@
 LOCAL_DIR=`pwd`
 export PATH=$PATH:$LOCAL_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 mkdir ../out
-make O=../out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- msm_defconfig
+make O=../out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- kiwi-64_defconfig
 make O=../out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- -j4
 
